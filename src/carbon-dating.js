8805b07ff987f1465d60555116102d1efc99595e
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-02-16 22:20:01 +0300

    feat: implement carbon-dating and count-cats

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,12 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof sampleActivity !== 'string' || typeof Number(sampleActivity) !== 'number' || Number(sampleActivity) <= 0 || Number(sampleActivity) > MODERN_ACTIVITY || /[a-zA-Z]/.test(sampleActivity)) {
+    return false;
+  } else {
+    return Math.ceil(
+      Math.log(MODERN_ACTIVITY / parseFloat(sampleActivity)) / (0.693 / HALF_LIFE_PERIOD)
+    )
+  }
 };
--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,11 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(backyard) {
+  let sum = 0;
+  for (let i = 0; i < backyard.length; i++) {
+    for (let j = 0; j < backyard[i].length; j++) {
+      if (backyard[i][j] === '^^') {
+        sum += 1;
+      }
+    }
+  }
+  return sum;
 };
