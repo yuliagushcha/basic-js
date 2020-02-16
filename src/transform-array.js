b3c8ac4bee0705e4fe9547111f009d5d1576ffc7
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-02-17 00:47:07 +0300

    feat: complete transform-array task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,27 @@
-module.exports = function transform(/* arr */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function transform(arr) {
+    if (!Array.isArray(arr) || typeof(arr) !== 'object') {
+        throw new Error();
+    } else if (!arr.length) {
+        return arr;
+    }
+
+    let newArr = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] === '--discard-next') {
+            i += 1;
+        } else if (arr[i] === '--discard-prev') {
+            if (newArr.length > 0) {
+                newArr.pop();
+            }
+        } else if (arr[i] === '--double-next') {
+            if (i < arr.length - 1) {
+                newArr.push(arr[i + 1]);
+            }
+        } else if (arr[i] === '--double-prev') {
+            if (i > 0) {
+                newArr.push(arr[i - 1]);
+            }
+        } else { newArr.push(arr[i])}
+    }
+    return newArr;
 };
