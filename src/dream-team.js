47f835ff8d9deb9f719f67fb75dc8a38531c3916
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-02-16 22:36:11 +0300

    feat: complete dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,13 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (typeof members !== 'object' || members === null) {
+    return false;
+  }
+  let arr = [];
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      let newArr = members[i].trim().split('');
+      arr.push(newArr[0].toUpperCase());
+    }
+  }
+  return arr.sort().join('');
 };
