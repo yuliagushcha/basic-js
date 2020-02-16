be9c30569e1f9de20c819e628082ba1843d7c0ac
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-02-17 00:03:35 +0300

    feat:  complete hanoi-tower task

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,13 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+  let obj = {
+    turns: 0,
+    seconds: 0
+  }
+
+  let newSpeed = turnsSpeed / 3600;
+
+  obj.turns = Math.pow(2, disksNumber);
+  obj.seconds = Math.pow(2, disksNumber) / newSpeed;
+
+  return obj;
 }
