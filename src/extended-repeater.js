95da4b39b5444d6e4727f74f9a66f115d59c4efd
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-03-07 22:13:58 +0300

    feat: complete extended-repeater task

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,34 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    var firstArr = [];
+    var secondArr = [];
+    if (options.repeatTimes !== undefined) {
+        if (options.additionRepeatTimes !== undefined) {
+        for (j = 0; j < options.additionRepeatTimes; j++) {
+            secondArr.push(String(options.addition));
+        }
+        }
+        for (i = 0; i < options.repeatTimes; i++) {
+        if (options.addition !== undefined) {
+            if (options.additionRepeatTimes !== undefined) {
+            if (options.additionSeparator !== undefined) {
+                firstArr.push(str + secondArr.join(`${options.additionSeparator}`));
+            } else {
+                firstArr.push(str + secondArr.join(""));
+            }
+            } else {
+            firstArr.push(str + options.addition);
+            }
+        } else {
+            firstArr.push(str);
+        }
+        }
+        if (options.separator !== undefined) {
+        return firstArr.join(`${options.separator}`);
+        } else {
+        return firstArr.join("+");
+        }
+    } else if (options.repeatTimes === undefined) {
+        return str + options.addition;
+    }
 };
   
