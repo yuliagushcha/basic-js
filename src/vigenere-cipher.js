ac1fba099425357536cd61c3448e9b699ccdf912
yuliagushcha <Yulia_Gushcha@mail.ru>
2020-03-07 22:16:25 +0300

    feat: complete vigenere-cipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,59 @@
 class VigenereCipheringMachine {
-    encrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    constructor(mode = true) {
+        this.mode = mode;
     }
 
-    decrypt() {
-        throw 'Not implemented';
-        // remove line with error and write your code here
+    encrypt(message, key) {
+        if (typeof message === 'undefined' || typeof key === 'undefined') {
+            throw new Error();
+        }
+        let data = {
+            result: [],
+            index: 0,
+        };
+        for (let i = 0; i < message.length; i++) {
+            if (message[i].match(/[A-Za-z]/)) {
+                let codeCharSymbol = message[i].toUpperCase().charCodeAt();
+                data.result[i] = String.fromCharCode(
+                    ( codeCharSymbol + (key[data.index % key.length].toUpperCase().charCodeAt())
+                        - 65 * 2) % 26 + 65);
+                data.index++;
+            } else {
+                data.result[i] = message[i];
+            }
+        }
+        if (this.mode) {
+            return data.result.join('');
+        } else {
+            return data.result.reverse().join('');
+        }
+    }
+
+    decrypt(encryptedMessage, key) {
+        if (typeof encryptedMessage === 'undefined' || typeof key === 'undefined') {
+            throw new Error();
+        }
+        let data = {
+            result: [],
+            index: 0,
+        };
+        for (let i = 0; i < encryptedMessage.length; i++) {
+            if (encryptedMessage[i].match(/[A-Za-z]/)) {
+                let codeCharSymbol = encryptedMessage[i].toUpperCase().charCodeAt();
+                data.result[i] = String.fromCharCode(Math.abs(
+                    codeCharSymbol - (key[data.index % key.length].toUpperCase().charCodeAt())
+                    + 26) % 26 + 65);
+                data.index++;
+            } else {
+                data.result[i] = encryptedMessage[i];
+            }
+        }
+        if (this.mode) {
+            return data.result.join('');
+        } else {
+            return data.result.reverse().join('');
+        }
+
     }
 }
-
 module.exports = VigenereCipheringMachine;
